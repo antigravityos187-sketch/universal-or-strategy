2a429c5e5b4052e31402f3ccd9e5365b401fee95
malhitticrypto <malhitticrypto@gmail.com>
2026-07-02 22:10:13 +0000

    Revert "fix(wave7/pr6): LogBuffer literal { dropped + DrawingHelpers UTC case missing"
    This reverts commit 3f517393799c473c19f108e625ea995843532172.

--- src/V12_002.DrawingHelpers.cs
+++ src/V12_002.DrawingHelpers.cs
@@ -83,8 +83,6 @@ namespace NinjaTrader.NinjaScript.Strategies
                     return TimeZoneInfo.FindSystemTimeZoneById("Mountain Standard Time");
                 case "Pacific":
                     return TimeZoneInfo.FindSystemTimeZoneById("Pacific Standard Time");
-                case "UTC":
-                    return TimeZoneInfo.Utc;
                 default:
                     return TimeZoneInfo.Local;
             }
